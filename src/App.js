f5d28a3f6a2b1e77df237522c5355fb6509140d6
Yuji T <accmanager1990@gmail.com>
2022-02-16 01:43:42 +0900

    App component with styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Header from './components/header/header';
+import './App.css';
+
+import { Outlet } from 'react-router';
+import useOrder from './data/order';
+
 function App() {
-  return <div className="App">Test</div>;
+  const order = useOrder();
+  return (
+    <div className="App">
+      <Header orderLength={order.getOrder().length} />
+      <Outlet context={[order]} />
+    </div>
+  );
 }
 
 export default App;
