11010fc19fa57d8734b0b499132e4e71d1a177a8
Yuji T <accmanager1990@gmail.com>
2022-02-16 01:46:35 +0900

    Add cart quantity indicator

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,24 @@
+import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './header.css';
-import { MdShoppingCart } from 'react-icons/md';
-const Header = () => {
+
+const Header = (props) => {
+  const { order, orderLength } = props;
+  useEffect(() => {
+    console.log('order changed: ', order);
+  }, [order]);
   return (
     <header className="header">
       <h1>Pixel Store</h1>
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/shop">Shop</NavLink>
-        <NavLink to="/cart">Cart</NavLink>
+        <NavLink to="/cart">
+          Cart
+          {orderLength ? (
+            <span className="order-count">{orderLength}</span>
+          ) : null}
+        </NavLink>
       </nav>
     </header>
   );
