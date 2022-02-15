51aa314c7381becdca2cd7dd29dfd609f04bc63f
Yuji T <accmanager1990@gmail.com>
2022-02-16 02:19:17 +0900

    Update to use orderHook and add styling

--- src/components/shop/items.js
+++ src/components/shop/items.js
@@ -2,34 +2,37 @@ import Item from './item';
 import { useSearchParams } from 'react-router-dom';
 import './items.css';
 
-const Items = (params) => {
-  const { items } = params;
+const Items = (props) => {
+  const { items, order } = props;
   const [searchParams] = useSearchParams();
+
   return (
     <div className="items">
       <p>
         Showing: <span>{searchParams.get('category') || 'All'}</span>{' '}
         {searchParams.get('category') ? 'category' : 'categories'}
       </p>
-      {items
-        .filter((item) => {
-          let filter = searchParams.get('filter');
-          if (filter) {
-            let name = item.name.toLowerCase();
-            return name.startsWith(filter.toLowerCase());
-          }
-          return true;
-        })
-        .filter((item) => {
-          let category = searchParams.get('category');
-          if (category) {
-            return item.category === category;
-          }
-          return true;
-        })
-        .map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
+      <div className="items-list">
+        {items
+          .filter((item) => {
+            let filter = searchParams.get('filter');
+            if (filter) {
+              let name = item.name.toLowerCase();
+              return name.startsWith(filter.toLowerCase());
+            }
+            return true;
+          })
+          .filter((item) => {
+            let category = searchParams.get('category');
+            if (category) {
+              return item.category === category;
+            }
+            return true;
+          })
+          .map((item) => (
+            <Item key={item.id} item={item} order={order} />
+          ))}
+      </div>
     </div>
   );
 };
